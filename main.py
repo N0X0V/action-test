48232afdccafa24c5f707caa9a3a30db0b913242
Quentin Berlioux <quentin.berlioux@student-cs.fr>
2023-05-01 14:13:10 +0200

    simple script

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def main():
+    print("""\
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \\
+                                  ,"  ## |   ಠ ಠ.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """)
+    requests.get("https://google.com")
+    print("> No error")
+
+
+if __name__ == "__main__":
+    main()
